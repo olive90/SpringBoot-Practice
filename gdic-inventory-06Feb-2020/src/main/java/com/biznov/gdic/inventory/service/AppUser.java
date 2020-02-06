d8e203c6148ba7462b24ada687a7faa5bce5ca08
Olive Hasan <olivehasan99@gmail.com>
2020-02-06 18:14:56 +0600

    sms finished working on inventory

---
+++ gdic-inventory-06Feb-2020/src/main/java/com/biznov/gdic/inventory/service/AppUser.java
@@ -0,0 +1,55 @@
+package com.biznov.gdic.inventory.service;
+
+public class AppUser {
+
+	private int id;
+	private String username;
+	private String password;
+	private String role;
+
+	public AppUser(int id, String username, String password, String role) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.role = role;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "AppUser [id=" + id + ", username=" + username + ", password=" + password + ", role=" + role + "]";
+	}
+
+}
---
+++ gdic-inventory-06Feb-2020/src/main/java/com/biznov/gdic/inventory/util/JwtUtil.java
@@ -0,0 +1,65 @@
+package com.biznov.gdic.inventory.util;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+
+	private SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+	public JwtUtil() {
+		super();
+	}
+
+	public String extractUserName(String token) {
+		return extractClaim(token, Claims::getSubject);
+	}
+
+	public Date extractExpiration(String token) {
+		return extractClaim(token, Claims::getExpiration);
+	}
+
+	public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+		final Claims claims = extarctAllClaims(token);
+		return claimsResolver.apply(claims);
+	}
+
+	private Claims extarctAllClaims(String token) {
+		return Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+	}
+
+	private Boolean isTokenExpired(String token) {
+		return extractExpiration(token).before(new Date());
+	}
+
+	public String generateToken(UserDetails userDetails) {
+		Map<String, Object> claims = new HashMap<>();
+		return createToken(claims, userDetails.getUsername());
+	}
+
+	@SuppressWarnings("deprecation")
+	private String createToken(Map<String, Object> claims, String subject) {
+		return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+				.signWith(SignatureAlgorithm.HS256, key).compact();
+	}
+	
+	public Boolean validateToken(String token, UserDetails userDetails) {
+		final String user = extractUserName(token);
+		return (user.equals(userDetails.getUsername()) && !isTokenExpired(token));
+	}
+
+}
